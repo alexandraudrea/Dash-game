a8a126e939e0a151136590d78654acf6cebdf6ad
alexandraudrea <alexandraudrea00@gmail.com>
2025-07-29 00:54:39 +0300

    remove base stuff from basic movement script

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,6 @@
 extends CharacterBody2D
 
-
 const SPEED = 150
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
